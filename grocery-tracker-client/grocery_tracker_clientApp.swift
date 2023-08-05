c1d7a695f8b15285136a05e1b6f9b719b0329002
Dayanand Balasubramanian <daya.bsm@gmail.com>
2023-08-05 16:21:38 +0100

    Initial Commit

---
+++ grocery-tracker-client/grocery_tracker_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocery_tracker_clientApp.swift
+//  grocery-tracker-client
+//
+//  Created by Dayanand Balasubramanian on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct grocery_tracker_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
